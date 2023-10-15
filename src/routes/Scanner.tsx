4c169cf3513b1f0f597c952fef62d0d56f83f7ea
Kenneth Yang <microbob@is88.com>
2023-10-15 06:06:14 -0700

    Scanner UI

--- src/routes/Scanner.tsx
+++ src/routes/Scanner.tsx
@@ -1,15 +1,29 @@
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import Grid from "@mui/material/Unstable_Grid2";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Checkbox,
+  FormControlLabel,
+  Button,
+} from "@mui/material";
 import MenuBar from "../components/MenuBar";
+import { useNavigate } from "react-router-dom";
 
 function Scanner() {
+  const navigate = useNavigate();
   const [isToggleOn, setIsToggleOn] = useState(false);
+  const items = [
+    { name: "SALSA VERDE", price: 2.29 },
+    { name: "EGGS LARGE CAGE FREE WHI", price: 2.59 },
+    { name: "SEMI SWEET CHOC CHIPS", price: 2.99 },
+    // Add more items here...
+  ];
 
   return (
     <>
       <MenuBar />
-      
+
       <h1>Scanner</h1>
       <input
         type="file"
@@ -23,15 +37,30 @@ function Scanner() {
 
       {isToggleOn && (
         <>
-          <Grid container spacing={2} alignItems="start">
-            <Grid xs={6}>SALSA VERDE</Grid>
-            <Grid xs={6}>$2.29</Grid>
-            <Grid xs={12}>Things</Grid>
-            <Grid xs={6}>EGGS LARGE CAGE FREE WHI</Grid>
-            <Grid xs={6}>$2.59</Grid>
-            <Grid xs={6}>SEMI SWEET CHOC CHIPS</Grid>
-            <Grid xs={6}>$2.99</Grid>
-          </Grid>
+          <List>
+            {items.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={item.name}
+                  secondary={`Price: $${item.price}`}
+                />
+                <FormControlLabel control={<Checkbox />} label="Daniel Pak" />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Virginia Wang"
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Jasmine Schoch"
+                />
+                <FormControlLabel control={<Checkbox />} label="Kenneth Yang" />
+              </ListItem>
+            ))}
+          </List>
+
+          <Button variant="contained" onClick={() => navigate("/pantry")}>
+            Submit to Pantry
+          </Button>
         </>
       )}
     </>
