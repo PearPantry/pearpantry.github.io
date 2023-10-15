48e95b742153ad7e27aa14316e356ea2e685ada2
Kenneth Yang <microbob@is88.com>
2023-10-15 06:01:31 -0700

    using grid

--- src/routes/Scanner.tsx
+++ src/routes/Scanner.tsx
@@ -1,16 +1,39 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Grid from "@mui/material/Unstable_Grid2";
+import MenuBar from "../components/MenuBar";
 
 function Scanner() {
-  const navigate = useNavigate();
+  const [isToggleOn, setIsToggleOn] = useState(false);
+
   return (
     <>
+      <MenuBar />
+      
       <h1>Scanner</h1>
       <input
         type="file"
         accept="image/*"
         capture="environment"
-        onChange={() => navigate("/pantry")}
+        onChange={() => setIsToggleOn(true)}
       />
+
+      <br />
+      <br />
+
+      {isToggleOn && (
+        <>
+          <Grid container spacing={2} alignItems="start">
+            <Grid xs={6}>SALSA VERDE</Grid>
+            <Grid xs={6}>$2.29</Grid>
+            <Grid xs={12}>Things</Grid>
+            <Grid xs={6}>EGGS LARGE CAGE FREE WHI</Grid>
+            <Grid xs={6}>$2.59</Grid>
+            <Grid xs={6}>SEMI SWEET CHOC CHIPS</Grid>
+            <Grid xs={6}>$2.99</Grid>
+          </Grid>
+        </>
+      )}
     </>
   );
 }
