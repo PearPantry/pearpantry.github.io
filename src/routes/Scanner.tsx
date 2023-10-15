95c948f68cb2db7c21056e193b2bc152dbdeb82d
Kenneth Yang <microbob@is88.com>
2023-10-15 06:10:37 -0700

    Scanner-ui (#11)
    * using grid
    
    * Scanner UI

--- src/routes/Scanner.tsx
+++ src/routes/Scanner.tsx
@@ -1,16 +1,68 @@
+import { useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Checkbox,
+  FormControlLabel,
+  Button,
+} from "@mui/material";
+import MenuBar from "../components/MenuBar";
 import { useNavigate } from "react-router-dom";
 
 function Scanner() {
   const navigate = useNavigate();
+  const [isToggleOn, setIsToggleOn] = useState(false);
+  const items = [
+    { name: "SALSA VERDE", price: 2.29 },
+    { name: "EGGS LARGE CAGE FREE WHI", price: 2.59 },
+    { name: "SEMI SWEET CHOC CHIPS", price: 2.99 },
+    // Add more items here...
+  ];
+
   return (
     <>
+      <MenuBar />
+
       <h1>Scanner</h1>
       <input
         type="file"
         accept="image/*"
         capture="environment"
-        onChange={() => navigate("/pantry")}
+        onChange={() => setIsToggleOn(true)}
       />
+
+      <br />
+      <br />
+
+      {isToggleOn && (
+        <>
+          <List>
+            {items.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={item.name}
+                  secondary={`Price: $${item.price}`}
+                />
+                <FormControlLabel control={<Checkbox />} label="Daniel Pak" />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Virginia Wang"
+                />
+                <FormControlLabel
+                  control={<Checkbox />}
+                  label="Jasmine Schoch"
+                />
+                <FormControlLabel control={<Checkbox />} label="Kenneth Yang" />
+              </ListItem>
+            ))}
+          </List>
+
+          <Button variant="contained" onClick={() => navigate("/pantry")}>
+            Submit to Pantry
+          </Button>
+        </>
+      )}
     </>
   );
 }
