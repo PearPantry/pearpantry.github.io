4eb6925e2d1da1de73872a23a458b6f117b5ac7f
Kenneth Yang <microbob@is88.com>
2023-10-15 01:56:19 -0700

    Reformat and fix import (#2)

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,18 +1,24 @@
-import { AppBar, Box, Container, Toolbar, Typography, Button } from '@mui/material';
-import AdbIcon from '@mui/icons-material/Adb';
-import { NavLink, useNavigate } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Container,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
+import AdbIcon from "@mui/icons-material/Adb";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const pages = ['Pantry', 'Scanner', 'Expenses'];
+const pages = ["Pantry", "Scanner", "Expenses"];
 
 function MenuBar() {
   const navigate = useNavigate();
-
 
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
             noWrap
@@ -20,28 +26,28 @@ function MenuBar() {
             to="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             PearPantry
-            </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Button
                 key={page}
                 onClick={() => navigate(page.toLowerCase())}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
                 {page}
               </Button>
             ))}
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -49,13 +55,13 @@ function MenuBar() {
             to="/"
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
             HI
