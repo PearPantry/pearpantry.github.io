16a6171f86cff35b116cd668152988a88a4d7355
Kenneth Yang <microbob@is88.com>
2023-10-15 09:17:18 -0700

    Donation (#17)
    * Home icon
    
    * Link to food bank
    
    * Moved file input
    
    * Add theme
    
    * Properly display nutrients
    
    * Remove redundant import

--- src/NutritionWidget.tsx
+++ src/NutritionWidget.tsx
@@ -1,44 +1,47 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const NutritionWidget = () => {
-  const [nutritionData, setNutritionData]: any = useState({});
-  const apiKey = 'a431318bd32245edb1d3ddebf242bb44';
+  const [nutritionData, setNutritionData]: any = useState(null);
+  const apiKey = "08ad4558f9f945aebde9c55ad1f54589";
 
   useEffect(() => {
     const id = 1095; // Spoonacular ID for blueberries
 
-    fetch(`https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=${apiKey}`)
-
+    fetch(
+      `https://api.spoonacular.com/recipes/${id}/nutritionWidget.json?apiKey=${apiKey}`
+    )
       .then((response) => {
         console.log(response);
 
-        return response.json()
+        return response.json();
       })
 
       .then((data) => {
         console.log(data);
         setNutritionData(data);
-
       })
       .catch((error) => {
-        console.error('Error fetching nutrition data:', error);
+        console.error("Error fetching nutrition data:", error);
       });
   }, []);
 
   return (
     <div>
-      <h2>Nutritional Data for Blueberries (per serving)</h2>
-      <ul>
-        <p>Calories: {nutritionData.calories}</p>
-        <p>Fat: {nutritionData.fat}</p>
-        <p>Saturated Fat: {nutritionData.saturatedFat}</p>
-        <p>Carbohydrates: {nutritionData.carbs}</p>
-        <p>Net Carbohydrates: {nutritionData.netCarbohydrates}</p>
-        <p>Sugar: {nutritionData.sugar}</p>
-        <p>Cholesterol: {nutritionData.cholesterol}</p>
-        <p>Sodium: {nutritionData.sodium}</p>
-        <p>Protein: {nutritionData.protein}</p>
-      </ul>
+      {nutritionData ? (
+        <ul>
+          <p>Calories: {nutritionData.calories}</p>
+          <p>Fat: {nutritionData.fat}</p>
+          <p>Saturated Fat: {nutritionData.nutrients[2].amount}</p>
+          <p>Carbohydrates: {nutritionData.carbs}</p>
+          <p>Net Carbohydrates: {nutritionData.nutrients[4].amount}</p>
+          <p>Sugar: {nutritionData.nutrients[5].amount}</p>
+          <p>Cholesterol: {nutritionData.nutrients[6].amount}</p>
+          <p>Sodium: {nutritionData.nutrients[7].amount}</p>
+          <p>Protein: {nutritionData.protein}</p>
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
