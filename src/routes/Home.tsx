2b6360b98206bfad7b7f14132e6aae807206c166
Jasmine Schoch <jasmineyschoch@gmail.com>
2023-10-15 03:28:05 -0700

    deleted unnecessary vars

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import '../styles/Home.css'
 import Greeting from '../components/Greeting';
 import MenuBar from '../components/MenuBar'
 import { Login } from './Login'
 import { auth } from '../scripts/firebase'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import HorizontalStack from '../components/HorizontalStack';
 
 
 function Home() {
-  const [count, setCount] = useState(0);
   const navigate = useNavigate();
 
   // Check if user is logged in and redirect to Auth route if not
